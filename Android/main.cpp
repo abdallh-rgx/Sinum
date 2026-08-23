156bf40cb564b23cf4b761750a166d9373a7250b
abdallh-rgx <abdallhrgxdevloper@gmail.com>
2026-08-23 11:00:02 +0300

    Update main.cpp

--- Android/main.cpp
+++ Android/main.cpp
@@ -1,45 +1,58 @@
 // Copyright (c) 2025 Project Nova LLC
+
 #include "Url.h"
 #include "Library.h"
 #include "Util.h"
 #include "Dobby/dobby.h"
 #include "Includes/curl.h"
 #include <dlfcn.h>
+
 #define URL_PROTOCOL_HTTP _("http")
 #define URL_HOST _("127.0.0.1:3551")
 #define URL_PORT std::string()
+
 install_hook_name(curl_easy_setopt, void*, void* curl, int option, void* arg)
 {
-if (!Util::IsPointerBad(arg) && option == CURLOPT_URL)
-{
-std::string url = reinterpret_cast<char*>(arg);
-Uri uri = Uri::Parse(url);
-if (uri.Host.ends_with(("ol.epicgames.com"))
-|| uri.Host.ends_with((".akamaized.net"))
-|| uri.Host.ends_with(_("on.epicgames.com")))
-{
-url = Uri::CreateUri(URL_PROTOCOL_HTTP, URL_HOST, URL_PORT, uri.Path, uri.QueryString);
+    if (!Util::IsPointerBad(arg) && option == CURLOPT_URL)
+    {
+        std::string url = reinterpret_cast<char*>(arg);
+
+        Uri uri = Uri::Parse(url);
+
+        if (uri.Host.ends_with(_("ol.epicgames.com"))
+            || uri.Host.ends_with(_(".akamaized.net"))
+            || uri.Host.ends_with(_("on.epicgames.com")))
+        {
+            url = Uri::CreateUri(URL_PROTOCOL_HTTP, URL_HOST, URL_PORT, uri.Path, uri.QueryString);
+        }
+
+        return orig_curl_easy_setopt(curl, option, (void*)url.c_str());
+    }
+    else if (option == CURLOPT_SSL_VERIFYPEER)
+    {
+        return orig_curl_easy_setopt(curl, option, (void*)0);
+    }
+
+    return orig_curl_easy_setopt(curl, option, arg);
 }
-return orig_curl_easy_setopt(curl, option, (void*)url.c_str());
-}
-else if (option == CURLOPT_SSL_VERIFYPEER)
-{
-return orig_curl_easy_setopt(curl, option, (void*)0);
-}
-return orig_curl_easy_setopt(curl, option, arg);
-}
+
 void* Main(void*)
 {
-Library::WaitFor(_("libUE4.so"));
-uintptr_t baseAddress = reinterpret_cast<uintptr_t>(Library::FindByName(_("libUE4.so")));
-if (!baseAddress) return nullptr;
-uintptr_t offset = 0x0E9A36AC;
-void* curl_easy_setopt = reinterpret_cast<void*>(baseAddress + offset);
-install_hook_curl_easy_setopt(curl_easy_setopt);
-return nullptr;
+    Library::WaitFor(_("libUE4.so"));
+
+    uintptr_t baseAddress = reinterpret_cast<uintptr_t>(Library::FindByName(_("libUE4.so")));
+    if (!baseAddress) return nullptr;
+
+    uintptr_t offset = 0x0E9A36AC;
+    void* curl_easy_setopt = reinterpret_cast<void*>(baseAddress + offset);
+
+    install_hook_curl_easy_setopt(curl_easy_setopt);
+
+    return nullptr;
 }
-attribute((constructor)) void libsinum_main()
+
+__attribute__((constructor)) void libsinum_main()
 {
-pthread_t ptid;
-pthread_create(&ptid, NULL, Main, NULL);
+    pthread_t ptid;
+    pthread_create(&ptid, NULL, Main, NULL);
 }
